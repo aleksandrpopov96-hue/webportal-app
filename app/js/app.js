995111a381c21eb17baa28280b03c157938fdb3d
Aleksandr Popov <ap@Aleksandrs-MacBook-Pro.local>
2026-07-28 14:38:24 +0300

    Fix proxy: use regex location instead of broken if+variable

--- app/js/app.js
+++ app/js/app.js
@@ -28,7 +28,7 @@
   const sidebar = document.getElementById('sidebar');
 
   function proxyUrl(url) {
-    return '/proxy/?url=' + encodeURIComponent(url);
+    return '/proxy/' + url;
   }
 
   function loadSites() {
